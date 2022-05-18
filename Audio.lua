3ccebbf4b31ee13826deb12ff399c6b90b69c9ac
PixelHeroFromLove2d <105568613+PixelHeroFromLove2d@users.noreply.github.com>
2022-05-18 12:41:16 -0700

    Added support for multiple background tracks.

--- Audio.lua
+++ Audio.lua
@@ -1,12 +1,34 @@
 --Audio
 a = {}
+
 function a.load()
-    source = love.audio.newSource( "Backgroundsong.wav", "stream" )
+
+    source1 = love.audio.newSource( "Backgroundsong1.wav", "stream" )
+    source2 = love.audio.newSource( "Backgroundsong2.wav", "stream" )
+    source3 = love.audio.newSource( "Backgroundsong3.wav", "stream" )
+    i = love.math.random( 1, 3 )
+
+    if i == 1 then
+        source = source1
+    elseif i == 2 then
+        source = source2
+    end
+
 end
 
 function a.update(dt)
+
+    
 	if not source:isPlaying( ) then
-	    love.audio.play( source )
+        i = love.math.random( 1, 3 )
+        if i == 1 then
+            source = source1
+        elseif i == 2 then
+            source = source2
+        elseif i == 3 then
+            source = source3
+        end
+        love.audio.play( source )
     end
 end
 

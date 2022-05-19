4e9701eef6ad3b600027f802d5fe461530f6ccbd
PixelHeroFromLove2d <105568613+PixelHeroFromLove2d@users.noreply.github.com>
2022-05-19 06:18:41 -0700

    Update Audio.lua

--- Audio.lua
+++ Audio.lua
@@ -6,7 +6,8 @@ function a.load()
     source1 = love.audio.newSource( "Backgroundsong1.wav", "stream" )
     source2 = love.audio.newSource( "Backgroundsong2.wav", "stream" )
     source3 = love.audio.newSource( "Backgroundsong3.wav", "stream" )
-    i = love.math.random( 1, 3 )
+    source3 = love.audio.newSource( "Backgroundsong3.wav", "stream" )
+    i = love.math.random( 1, 4 )
 
     if i == 1 then
         source = source1
@@ -20,13 +21,15 @@ function a.update(dt)
 
     
 	if not source:isPlaying( ) then
-        i = love.math.random( 1, 3 )
+        i = love.math.random( 1, 4 )
         if i == 1 then
             source = source1
         elseif i == 2 then
             source = source2
         elseif i == 3 then
             source = source3
+	elseif i == 4 then
+            source = source4
         end
         love.audio.play( source )
     end
